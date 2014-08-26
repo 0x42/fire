713c24c94267d8d15b38722df451dcb7786b452a
0x42 <bpozdnyakov.gmail.com>
2014-08-26 17:31:56 +0400

    modified:   src/log/logging.c 	modified:   src/log/logging.h

--- src/log/logging.c
+++ src/log/logging.c
@@ -2,15 +2,43 @@
 // флаг проверки инициализации
 static int initFlg = 0;
 // имя лог файла
-static char *logFileName;
+static char *logFileName = 0;
 
 // иниц-ия получение имени файла для логирования
 void loggingINIT()
 {
 	debugPrint("loggingINIT run\n");
 	initFlg = 1;
-	logFileName = "log.conf";
+	logFileName = "fire.log";
 	debugPrint("initFlg = %d\nlogFileName = %s\n", initFlg, logFileName);
+}
+
+// выводи в logFileName инфор - ое сообщение
+void loggingINFO(char *msg)
+{
+	char *head = " INFO ";
+	if(initFlg) {
+		FILE *file = fopen(logFileName, "a+");
+		if(file != NULL) {
+		
+
+		} else {
+			char *errTxt = strerror(errno);
+			debugPrint(errTxt);
+		}
+	} else {
+		char *err = "loggingINFO() - SYSTEM-ERROR: \
+			     run loggingINFO() before loggingINIT() when try write:\n";
+		loggingERROR(err);
+		loggingERROR(msg);
+	}
+}
+
+// выводим error сообщение в лог
+void loggingERROR(char *msg)
+{
+	char *head = " ERROR ";
+	printf("%s->%s", head, msg);
 }
 
 void loggingFIN()
--- src/log/logging.h
+++ src/log/logging.h
@@ -1,6 +1,7 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 #include <stdio.h>
+#include <errno.h>
 #include "tools.h"
 
 void loggingINIT();
