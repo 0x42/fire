693d2cd6d47c69ef437657b0e2a266416e7904ed
0x42 <bpozdnyakov.gmail.com>
2014-09-24 17:05:34 +0400

    new file:   src/nettcp/bo_net.c 	new file:   src/nettcp/bo_net.h

---
+++ src/nettcp/bo_net.c
@@ -0,0 +1,51 @@
+#include "bo_net.h"
+/*
+ * @brief		откр сокет
+ * @return		возвращает socket или -1 вслучае ошибки 
+ */
+int initServerSock()
+{
+	int sock = 0;
+	int ans = 1;
+	struct sockaddr_in saddr;
+	int port = 8888;
+	/* PF_INET - интернет сокет IP4 
+	 * SOCK_STREAM - потоковый сокет(TCP)
+	 * SOCK_DGRAM  - дейтагр(UDP)
+	 */
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock == -1) {
+		ans = -1;
+	} else {
+		memset(&saddr, 0, sizeof(struct sockaddr_in));
+		/*Семейство протокола ip4*/
+		saddr.sin_family = AF_INET;
+		/* порт */
+		saddr.sin_port = htons(port);
+		/* address ip4 */
+		saddr.sin_addr.s_addr = INADDR_ANY;
+		if(bind(sock, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
+			ans = -1;
+		} else {
+			ans = sock;
+		} 
+	}
+	return ans;
+}
+/* ----------------------------------------------------------------------------
+ * @brief	слушаем сокет sock, как только приходит коннект возвр сокет 
+ *		связ с клиентом clientfd;
+ * @return	1 - ok; -1 - error
+ */
+int waitConnect(int sock, int *clientfd)
+{
+	int ans = 1;
+	int queue_len = 2;
+	if(listen(sock, 2) == -1) ans = -1;
+	else {
+	/// Wait and Accept connection
+		*clientfd = accept(sock, NULL, NULL);
+		if(*clientfd == -1) ans = -1;
+	}
+	return ans;
+}
---
+++ src/nettcp/bo_net.h
@@ -0,0 +1,11 @@
+#ifndef BO_NET_H
+#define	BO_NET_H
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+
+#endif	/* BO_NET_H */
+
+/* 0x42 */
