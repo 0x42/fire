40071fc6751a2697e6c3da66070e6b453e1b9871
0x42 <bpozdnyakov.gmail.com>
2014-10-29 10:26:16 +0300

    new file:   master/Makefile 	new file:   master/main.c

---
+++ master/main.c
@@ -0,0 +1,22 @@
+/* ----------------------------------------------------------------------------
+ * Опрос устройств указ в конф файле
+ * получаем изменения -> сохр в таблицу -> отпр изм-ия устр-м из списка 
+ */
+
+#include <mcheck.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../src/tools/dbgout.h"
+#include "../src/log/bologging.h"
+
+extern void bo_master_main(int argc, char **argv);
+
+int main(int argc, char **argv)
+{
+	mtrace();
+	dbgout("START master-> %s\n", *argv);
+	bo_master_main(argc, argv);
+	dbgout("END\n");
+	muntrace();
+	return 0;
+}
