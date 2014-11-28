bb474e6ccc678f5d6a48195e9ab3a4e3690d9b39
0x42 <bpozdnyakov.gmail.com>
2014-11-28 11:12:55 +0300

    modified:   src/nettcp/bo_net_fifo_server.c

--- src/nettcp/bo_net_fifo_server.c
+++ src/nettcp/bo_net_fifo_server.c
@@ -555,7 +555,7 @@ static int bo_checkDblMsg(struct ParamSt *param)
 	char *tab_id = NULL;
 	exec = bo_getIp(param->clientfd, ip);
 	if(exec == 1) {
-		printf("TAB  ID MSG \n");
+	/*	printf("TAB  ID MSG \n");
 		sss = ht->size;
 		for(ii_ht = 0; ii_ht < sss; ii_ht++) {
 			key = *(ht->key + ii_ht);
@@ -565,6 +565,7 @@ static int bo_checkDblMsg(struct ParamSt *param)
 			}
 		}
 		printf("===========\n");
+	*/
 		printf("IP[%s]", ip);
 		tab_id = ht_get(param->id_msg, ip, tab_id);
 		printf("tab_id[%s]\n", tab_id);
